5257181ab7557673bf1366ff96e88d6491208adf
Difermacru <dfmarinoc@gmail.com>
2025-01-30 07:18:06 -0300

    correccion index

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,7 @@ const router = createRouter({
         {
           path:'productos/seeder',
           name:'seed-products',
-          component: () => import('../views/admin/SeederView.vue')
+          component: () => import('../views/admin/seederView.vue')
         },
         {
           path:'Ventas',
